95807235f34a9331e1f9df5e0cb99eba10450001
Daniel Domingo-Fernández <danieldomingofernandez@hotmail.com>
2020-03-06 16:05:36 +0100

    Update cli.py

--- src/diffupath/cli.py
+++ src/diffupath/cli.py
@@ -15,8 +15,56 @@ def main():
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 
 
-# TODO Consider which commands could be implemented, since for now all the package functionalities are
-#  offered as functions imports.
+
+@main.command()
+@click.option(
+    '-n', '--network',
+    help='Path to the network graph or kernel',
+    required=True,
+    type=click.Path(exists=True, dir_okay=False)
+)
+@click.option(
+    '-i', '--input',
+    help='Input data',
+    required=True,
+    type=click.Path(exists=True, dir_okay=False)
+)
+@click.option(
+    '-o', '--output',
+    help='Output path for the results',
+    default=OUTPUT,
+    show_default=True,
+    type=click.Path(exists=True, file_okay=False)
+)
+@click.option(
+    '-m', '--method',
+    help='Difussion method',
+    type=click.Choice(METHODS),
+)
+def diffuse(
+        network: str,
+        input: str,
+        output: str,
+        method: str,
+):
+    """Run a diffusion method over a network or pregenerated kernel."""
+    raise NotImplementedError
+    #TODO : Process arguments and call diffuse
+
+    
+@main.command()
+@click.option(
+    '-d', '--database',
+    help='Database to be downloaded',
+    required=True,
+)
+def download_database(
+        database: str,
+):
+    """Downloads a biological database and prepare the network for diffusion."""
+    raise NotImplementedError
+    #TODO : Process arguments and call diffuse
+
 
 if __name__ == '__main__':
     main()
