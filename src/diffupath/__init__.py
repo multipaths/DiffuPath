47a9385ce1f79d8ec570b0bed1b7084720e850c5
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2020-03-13 14:09:35 +0100

    Update docs

--- src/diffupath/__init__.py
+++ src/diffupath/__init__.py
@@ -1,32 +1,3 @@
 # -*- coding: utf-8 -*-
 
-"""
-The goal of this package is to re-implement the Diffusion algorithm described in ... in Python.
-TODO: Add me
-
-Installation
-------------
-1. ``diffupath`` can be installed with the following commmands:
-
-.. code-block:: sh
-
-    python3 -m pip install ...
-
-2. or in editable mode with:
-
-.. code-block:: sh
-
-    git clone ..
-
-.. code-block:: sh
-
-    cd diffupath
-
-.. code-block:: sh
-
-    python3 -m pip install -e .
-
-How to use
-----------
-
-"""
+""""DiffuPath."""
