28b8f33d36353cf7e63199ccbd4387251503f8ae
jmarinllao <josepmarinllao@gmail.com>
2020-04-01 19:07:48 +0200

    Cli run diffusion input treatment kernels and graph refactor

--- src/diffupath/cli.py
+++ src/diffupath/cli.py
@@ -12,6 +12,7 @@ from diffupath.utils import get_labels_set_from_dict
 from diffupath.validation_datasets_parsers import parse_set1
 
 from diffupy.diffuse import diffuse as run_diffusion
+from diffupy.kernels import regularised_laplacian_kernel
 from diffupy.process_input import generate_categoric_input_from_labels
 from diffupy.utils import process_network_from_cli, process_kernel_from_cli, print_dict_dimensions, \
     get_label_list_graph
@@ -33,22 +34,30 @@ def diffusion():
 
 @diffusion.command()
 @click.option(
-    '-n', '--network',
-    help='Path to the network graph or kernel',
-    required=True,
-    type=click.Path(exists=True, dir_okay=False)
-)
-@click.option(
     '-i', '--input',
     help='Input data',
     required=True,
     type=click.Path(exists=True, dir_okay=False)
 )
 @click.option(
+    '-n', '--network',
+    help='Path to the network graph or kernel',
+    default=os.path.join(DEFAULT_DIFFUPY_DIR, 'kernels', 'kernel_regularized_pathme_universe.pickle'),
+    show_default=True,
+    type=click.Path(exists=True, dir_okay=False)
+)
+@click.option(
+    '-g', '--graph',
+    help='Network provided as a graph',
+    show_default=False,
+    is_flag=False
+)
+@click.option(
     '-m', '--method',
     help='Difussion method',
+    default='raw',
+    show_default=True,
     type=click.Choice(DIFFUPY_METHODS),
-    required=True,
 )
 @click.option(
     '-o', '--output',
@@ -57,33 +66,28 @@ def diffusion():
     show_default=True,
     type=click.Path(exists=True, file_okay=False)
 )
-@click.option(
-    '-k', '--kernel',
-    help='Network provided as a kernel',
-    is_flag=False
-)
 def run(
-        network: str,
         input: str,
-        method: str,
+        network: str = os.path.join(DEFAULT_DIFFUPY_DIR, 'kernels', 'kernel_regularized_pathme_universe.pickle'),
+        graph: bool = False,
+        method: str = 'raw',
         output: str = OUTPUT_DIR,
-        kernel: bool = False,
 ):
     """Run a diffusion method over a network or pregenerated kernel."""
     click.secho(f'{EMOJI} Running diffusion... {EMOJI}')
 
     click.secho(f'{EMOJI} Loading graph from {network} {EMOJI}')
 
-    #TODO: Universal input processing
-    #input_scores = _process_input(input)
-    dataset1_labels_by_omics = parse_set1(input)
-    dataset1_all_labels = get_labels_set_from_dict(dataset1_labels_by_omics)
+    # TODO: Universal input processing, now parse set 1 specific, use from diffuPy process_input
+    # input_scores = _process_input(input)
+    dataset_labels_by_omics = parse_set1(input)
+    dataset1_all_labels = get_labels_set_from_dict(dataset_labels_by_omics)
 
-    print_dict_dimensions(dataset1_labels_by_omics, 'Dataset1 imported labels:')
+    print_dict_dimensions(dataset_labels_by_omics, 'Dataset1 imported labels:')
 
-    mirnas_dataset =  dataset1_labels_by_omics['micrornas']
+    mirnas_dataset = dataset_labels_by_omics['micrornas']
 
-    if not kernel:
+    if graph:
         graph = process_network_from_cli(network)
 
         click.secho(
@@ -93,51 +97,36 @@ def run(
             f'{EMOJI}'
         )
 
-        background_labels = set(get_label_list_graph(graph, 'name'))
-
-        mapping_scores = get_mapping(dataset1_all_labels, background_labels, title = 'Global mapping: ', mirnas = mirnas_dataset, print_percentage = True)
-
-        input_scores = generate_categoric_input_from_labels(
-            mapping_scores,
-            'input scores',
-            #TODO: a kernel should be provided not a label list, consider if treat it inside run_diffusion
-            background_labels
-        )
-
-        results = run_diffusion(
-            input_scores,
-            method,
-            graph,
-        )
+        k = regularised_laplacian_kernel(graph)
 
     else:
         k = process_kernel_from_cli(network)
 
-        background_labels = k.rows_labels
-
-        mapping_scores = get_mapping(dataset1_all_labels,
-                                     background_labels,
-                                     title = 'Global mapping: ',
-                                     mirnas = mirnas_dataset,
-                                     print_percentage = True
-                                     )
-
-        input_scores = generate_categoric_input_from_labels(mapping_scores,
-                                                            'input with hidden true positives',
-                                                            k
-                                                            )
-
         click.secho(
             f'{EMOJI} Kernel loaded with: \n'
-            f'{len(background_labels)} nodes\n'
+            f'{len(k.rows_labels)} nodes\n'
             f'{EMOJI}'
         )
 
-        results = run_diffusion(
-            input_scores,
-            method,
-            k=k,
-        )
+    background_labels = k.rows_labels
+
+    mapping_scores = get_mapping(dataset1_all_labels,
+                                 background_labels,
+                                 title='Global mapping: ',
+                                 mirnas=mirnas_dataset,
+                                 print_percentage=True
+                                 )
+
+    input_scores = generate_categoric_input_from_labels(mapping_scores,
+                                                        'input with hidden true positives',
+                                                        k
+                                                        )
+
+    results = run_diffusion(
+        input_scores,
+        method,
+        k=k,
+    )
 
     results.to_csv(output)
 
