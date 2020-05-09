b5bbc88332f5307b34dc73c05d6e1af512915e9e
jmarinllao <josepmarinllao@gmail.com>
2020-05-09 19:53:19 +0200

    DiffuPath views statistical test fdr_barchart added and general refactor and cleaning

--- src/diffupath/views.py
+++ src/diffupath/views.py
@@ -2,32 +2,163 @@
 
 """Visualization methods."""
 
-import chart_studio.plotly as py
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import plotly.graph_objs as go
+
+from diffupath.cross_validation import get_normalized_p_values
+
 from matplotlib_venn import venn3
 
 
-def heatmap(
-        data, row_labels, col_labels, ax=None, cbar_kw=None, cbarlabel="", title="", **kwargs
+def show_box_plot(
+        data_dict,
+        x_label='',
+        y_label='',
+        color_palette=None
+):
+    """Plot boxplot."""
+    if color_palette is None:
+        color_palette = ['royalblue', 'forestgreen', 'khaki', 'lightcoral', 'yellow', 'green']
+
+    bplots = []
+
+    fig, axs = plt.subplots(nrows=1,
+                            ncols=len(data_dict),
+                            figsize=(18, 6)
+                            )
+    if not isinstance(axs, np.ndarray):
+        axs = [axs]
+
+    for i, (dataset_label, dataset) in enumerate(data_dict.items()):
+        # rectangular box plot
+        bplot = axs[i].boxplot(list(dataset.values()),
+                               vert=True,  # vertical box alignment
+                               patch_artist=True,  # fill with color
+                               labels=list(dataset.keys())
+                               )  # will be used to label x-ticks
+        axs[i].set_title(dataset_label)
+
+        bplots.append(bplot)
+
+    # fill with colors
+    for bplot in bplots:
+        for i, (patch, color) in enumerate(zip(bplot['boxes'], color_palette)):
+            patch.set_facecolor(color_palette[i])
+
+    # adding horizontal grid lines
+    for i, ax in enumerate(axs):
+        ax.yaxis.grid(True)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+
+    plt.show()
+
+
+def fdr_barchart_three_plot(
+        metrics,
+        p_values_func,
+        title='Statistic Test',
+        legend=None
+):
+    if legend is None:
+        if isinstance(metrics, dict):
+            legend = list(metrics.keys())
+        else:
+            legend = ('Dataset 1', 'Dataset 2', 'Dataset 3')
+
+    if isinstance(metrics, dict):
+        metrics = list(metrics.values())
+
+    plt.rcParams.update({'font.size': 17})
+
+    normalized_p_values = []
+
+    for metric in metrics:
+        p_values = p_values_func(metric)
+        normalized_p_values.append(list(get_normalized_p_values(p_values).values()))
+        x = list(p_values.keys())
+
+    ind = np.arange(len(x))  # the x locations for the groups
+    width = 0.27  # the width of the bars
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    fig.set_size_inches(14.5, 7.5)
+
+    rects1 = ax.bar(ind, normalized_p_values[0], width, color='forestgreen')
+    rects2 = ax.bar(ind + width, normalized_p_values[1], width, color='tomato')
+    rects3 = ax.bar(ind + width * 2, normalized_p_values[2], width, color='steelblue')
+
+    ax.set_ylabel("normalized FDR -log10(p-value)")
+    plt.xlabel(title)
+
+    ax.set_xticks(ind + width)
+    ax.set_xticklabels(x, ha='left', rotation=-45)
+    ax.legend((rects1[0], rects2[0], rects3[0]), legend)
+
+    ax.plot([-0.2, 6], [-np.math.log10(0.05), -np.math.log10(0.05)], "k--")
+
+    plt.show()
+
+
+def show_venn_diagram(
+        intersections,
+        set_labels=('KEGG', 'Reactome', 'WikiPathways')
+):
+    """Show venn diagram."""
+    intersections_len = [len(intersection) for name, intersection in intersections.items()]
+
+    plt.figure(figsize=(17, 8))
+    v = venn3(subsets=intersections_len, set_labels=set_labels)
+
+    plt.show()
+
+
+def show_heatmap(
+        entity_number,
+        entity_count,
+        databases,
+        entity_types,
+        title="DiffuPath Mapping"
+):
+    fig, ax = plt.subplots(figsize=(15, 7))
+
+    im, cbar = _generate_heatmap(entity_number, databases, entity_types, ax=ax,
+                                 cmap="YlGn", cbarlabel="percentage [0-1]")
+    _ = _annotate_heatmap(im, entity_count=entity_count, valfmt="{x:1} ")
+
+    fig.tight_layout()
+    ax.set_title(title)
+
+    plt.show()
+
+
+def _generate_heatmap(
+        data,
+        row_labels,
+        col_labels,
+        ax=None,
+        cbar_kw={},
+        cbarlabel="",
+        title="",
+        **kwargs
 ):
     """Create a heatmap from a numpy array and two lists of labels.
 
-    Optional parameters: ax: A matplotlib.axes.Axes instance to which the heatmap is plotted. If not provided, use
-    current axes or create a new one. cbar_kw: A dictionary with arguments to :meth:`matplotlib.Figure.colorbar`.
-    cbarlabel: The label for the colorbar.
+     Optional parameters: ax: A matplotlib.axes.Axes instance to which the heatmap is plotted. If not provided, use
+     current axes or create a new one. cbar_kw: A dictionary with arguments to :meth:`matplotlib.Figure.colorbar`.
+     cbarlabel: The label for the colorbar.
 
-    :param data: A 2D numpy array of shape (N,M)
-    :param row_labels: A list or array of length N with the labels for the rows
-    :param col_labels: A list or array of length N with the labels for the columns
-    :param ax: axis
-    :param cbar_kw: kwars for cbar
-    :param cbarlabel: label
-    :param title: title
-    """
-
+     :param data: A 2D numpy array of shape (N,M)
+     :param row_labels: A list or array of length N with the labels for the rows
+     :param col_labels: A list or array of length N with the labels for the columns
+     :param ax: axis
+     :param cbar_kw: kwars for cbar
+     :param cbarlabel: label
+     :param title: title
+     """
     if not ax:
         ax = plt.gca()
 
@@ -67,7 +198,7 @@ def heatmap(
     return im, cbar
 
 
-def annotate_heatmap(
+def _annotate_heatmap(
         im,
         data=None,
         valfmt="{x:.2f}",
@@ -114,90 +245,3 @@ def annotate_heatmap(
             texts.append(text)
 
     return texts
-
-
-def show_heatmap(count, percentage, databases, entity_types):
-    """Show heatmap"""
-    fig, ax = plt.subplots(figsize=(15, 7))
-
-    im, cbar = heatmap(percentage, databases, entity_types, ax=ax,
-                       cmap="YlGn", cbarlabel="percentage [0-1]")
-    texts = annotate_heatmap(im, entity_count=count, valfmt="{x:1} ")
-
-    fig.tight_layout()
-    ax.set_title("Dataset 1 DiffuPath Mapping")
-
-    plt.show()
-
-
-def box_plot_from_dict(d, title='Box plot', x_title='x', y_title='y'):
-    """Plot boxplot."""
-    data = [go.Box(
-        y=v,
-        name=k
-    ) for k, v in d.items()
-    ]
-
-    layout = go.Layout(
-        title=go.layout.Title(
-            text=title,
-            xref='paper'
-        ),
-        xaxis=go.layout.XAxis(
-            title=go.layout.xaxis.Title(
-                text=x_title
-            )
-        ),
-        yaxis=go.layout.YAxis(
-            title=go.layout.yaxis.Title(
-                text=y_title
-            )
-        )
-    )
-
-    fig = go.Figure(data=data, layout=layout)
-
-    return py.iplot(fig)
-
-
-def box_plot_from_two_dimension_dict(d, y_label='y'):
-    """Boxplot two dimensions."""
-    x = [k2 for k1, v1 in d.items() for k2, v2 in v1.items() for i in range(len(v2))]
-
-    color_palete = ['#FF4136', '#3d9970', '#344f9e', '#bfc464']
-    color_dict = {}
-    i = 0
-
-    for k, v in d.items():
-        color_dict[k] = color_palete[i]
-        i += 1
-
-    data = [go.Box(
-        y=[i for k2, v2 in v1.items() for i in v2],
-        x=x,
-        name=k1,
-        marker=dict(
-            color=color_dict[k1]
-        )
-    ) for k1, v1 in d.items()]
-
-    layout = go.Layout(
-        yaxis=dict(
-            title=y_label,
-            zeroline=False
-        ),
-        boxmode='group'
-    )
-    fig = go.Figure(data=data, layout=layout)
-
-    return py.iplot(fig)
-
-
-def show_venn_diagram(intersections, set_labels=('KEGG', 'Reactome', 'WikiPathways')):
-    """Show venn diagram."""
-    intersections_len = [len(intersection) for name, intersection in intersections.items()]
-
-    plt.figure(figsize=(17, 8))
-    v = venn3(subsets=intersections_len, set_labels=set_labels)
-
-    plt.show()
