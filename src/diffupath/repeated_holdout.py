4ab1b55f8179e9ae7b9ef25b480677c4a18cdb49
jmarinllao <josepmarinllao@gmail.com>
2020-10-01 17:22:52 +0200

    Leave Two Omics Out validation added and validation code rearrangement

--- src/diffupath/repeated_holdout.py
+++ src/diffupath/repeated_holdout.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Cross-validation utilities."""
-import itertools
-import math
 from collections import defaultdict
 from typing import Union, Tuple
 
 import numpy as np
-import pandas as pd
 from diffupy.diffuse_raw import diffuse_raw
 from diffupy.matrix import Matrix
 from diffupy.process_input import format_input_for_diffusion, process_input_data, \
     _type_dict_label_scores_dict_data_struct_check, _type_dict_label_list_data_struct_check, map_labels_input
-from scipy import stats
-from scipy.stats import wilcoxon
 from sklearn import metrics
-from statsmodels.stats.multitest import fdrcorrection
 from tqdm import tqdm
 
 from .topological_analyses import generate_pagerank_baseline
@@ -97,7 +91,9 @@ def cross_validation_by_subgraph(mapping_input,
             if type in tmp_mapping.keys():
                 data_input_i = tmp_mapping[type]
 
-            elif (_type_dict_label_list_data_struct_check(mapping_input) or _type_dict_label_scores_dict_data_struct_check(mapping_input)) and type in mapping_input:
+            elif (_type_dict_label_list_data_struct_check(
+                    mapping_input) or _type_dict_label_scores_dict_data_struct_check(
+                    mapping_input)) and type in mapping_input:
                 data_input_i = mapping_input[type]
 
             else:
@@ -193,71 +189,3 @@ def _get_metrics(validation_labels,
     """Return metrics."""
     return metrics.roc_auc_score(validation_labels.mat, scores.mat), metrics.average_precision_score(
         validation_labels.mat, scores.mat)
-
-
-"""Statistical test"""
-
-
-def get_p_values(metrics):
-    """Get p_values."""
-    p_values = {}
-
-    result_list = map(dict, itertools.combinations(metrics.items(), 2))
-
-    for ch in result_list:
-        values = list(ch.values())
-        ttest = stats.ttest_rel(a=values[0], b=values[1])
-        p_values[str(tuple(ch.keys()))] = ttest.pvalue
-
-    return p_values
-
-
-def get_p_values_multiple(metrics):
-    """Get p_values_multiple."""
-    p_values = {}
-
-    for k, v in metrics[0].items():
-        ttest = stats.ttest_rel(a=v, b=metrics[1][k])
-        p_values[str(k)] = ttest.pvalue
-
-    return p_values
-
-
-def get_normalized_p_values(p_values):
-    """Get normalized_p_values."""
-    normalized_p_values = {}
-
-    fdr = fdrcorrection(list(p_values.values()),
-                        alpha=0.05,
-                        method='indep',
-                        is_sorted=False)
-
-    for k1, k2 in enumerate(p_values.keys()):
-        normalized_p_values[k2] = -math.log10(fdr[1][k1])
-
-    return normalized_p_values
-
-
-def get_wilcoxon_test(metrics, dataframe=False, title=""):
-    """Get Wilcoxon test p_values."""
-    p_values = defaultdict(lambda: defaultdict(lambda: float))
-
-    for metrics_i in metrics:
-        result_list = map(dict, itertools.combinations(metrics[metrics_i].items(), 2))
-        for ch in result_list:
-            values = list(ch.values())
-
-            _, p_value = wilcoxon(values[0], values[1])
-
-            p_values[metrics_i][str(tuple(ch.keys()))] = p_value
-
-    if dataframe:
-        df = pd.DataFrame.from_dict({(i, j): [i, j, p_values[i][j], p_values[i][j] < 0.017]
-                                     for i in p_values.keys()
-                                     for j in p_values[i].keys()},
-                                    orient='index')
-        df.columns = ['Dataset', f'Comparison {title}', 'p_value', 'Significant difference']
-
-        return df
-
-    return p_values
